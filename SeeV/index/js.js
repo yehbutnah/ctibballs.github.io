02a3efe1e1342601820e93a67c11b3078219b60e
Ctibballs <christibballs1990@gmail.com>
2016-04-15 12:33:34 +1000

    seev

---
+++ SeeV/index/js.js
@@ -0,0 +1,11 @@
+   	function resettoggle() {
+		var e = document.getElementById('drop-menu');
+		e.style.display = 'none';
+	}
+    function toggle_visibility(id) {
+       var e = document.getElementById(id);
+       if(e.style.display == 'block')
+          e.style.display = 'none';
+       else
+          e.style.display = 'block';
+    }
